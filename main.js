1ec2017ed1bc4e72e478c0923fa766930154f2a4
RheannaBG <rheenadss@gmail.com>
2024-07-16 10:36:19 +0100

    bg

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Software Developer","Massage Therapist"," Basketball Coach" ], 
+    typedSpeed:100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
